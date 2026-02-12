155910f9f1212466ed1bd9f4b39c90bc5d51ffcd
Francisco Javier Gallego Lahera <fgallego19@hotmail.com>
2026-02-12 11:49:40 +0100

    Added missing commit

--- ios/VibeTag/Core/Data/Repositories/AppleMusicLibraryRepositoryImpl.swift
+++ ios/VibeTag/Core/Data/Repositories/AppleMusicLibraryRepositoryImpl.swift
@@ -10,9 +10,7 @@ class AppleMusicLibraryRepositoryImpl: MusicLibraryRepository {
         let response = try await request.response()
         
         // 2. Create the playlist
-//        let playlist = try await MusicLibrary.shared.createPlaylist(name: name, description: description)
         
-        // 3. Add songs to the playlist
-        try await MusicLibrary.shared.createPlaylist(name: "Test", items: response.items)
+        try await MusicLibrary.shared.createPlaylist(name: name, items: response.items)
     }
 }
